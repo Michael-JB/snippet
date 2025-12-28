7d9a210014e33bf5ad3f98693f365ba2714b3a41
Michael Barlow <michael@ramyun.co.uk>
2025-12-28 10:29:11 +0100

    gracefully handle invalid URLs and empty content

--- index.js
+++ index.js
@@ -4,18 +4,34 @@ const getSnippet = () => document.getElementById("snippet");
 
 const addInputListener = () => {
   const snippet = getSnippet();
-  const onInput = debounce(async () => saveText(snippet), 200);
+  const onInput = debounce(() => saveText(snippet), 200);
   snippet.addEventListener("input", onInput);
 };
 
 const loadText = async () => {
-  snippet = getSnippet();
+  const snippet = getSnippet();
   snippet.focus();
+
   const payload = readURL();
-  snippet.textContent = payload ? await decode(payload) : "";
+  const decoded = payload ? await decode(payload) : undefined;
+
+  if (payload && !decoded) {
+    console.info("Invalid or corrupt URL payload. Double-check your link.");
+    clearURL();
+  }
+
+  snippet.textContent = decoded ?? "";
 };
 
-const saveText = async (snippet) => writeURL(await encode(snippet.textContent));
+const saveText = async (snippet) => {
+  if (!snippet.textContent) {
+    clearURL();
+    return;
+  }
+  writeURL(await encode(snippet.textContent));
+};
+
+const clearURL = () => history.replaceState(null, "", location.pathname);
 
 const writeURL = (payload) => {
   const hash = "#" + payload;
@@ -34,10 +50,14 @@ const encode = async (text) => {
   return new Uint8Array(buffer).toBase64({ alphabet: "base64url" });
 };
 
-const decode = async (encodedBytes) => {
-  const bytes = Uint8Array.fromBase64(encodedBytes, { alphabet: "base64url" });
-  const buffer = await feed(bytes, new DecompressionStream("deflate-raw"));
-  return new TextDecoder().decode(buffer);
+const decode = async (encoded) => {
+  try {
+    const bytes = Uint8Array.fromBase64(encoded, { alphabet: "base64url" });
+    const buffer = await feed(bytes, new DecompressionStream("deflate-raw"));
+    return new TextDecoder("utf-8", { fatal: true }).decode(buffer);
+  } catch (e) {
+    return null;
+  }
 };
 
 const feed = async (bytes, stream) => {
